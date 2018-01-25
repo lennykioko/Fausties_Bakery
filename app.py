ad79bb9f40146b3649ba42fa0ff219e2130389cf
unknown <lennykmutua@gmail.com>
2018-01-25 15:14:40 +0300

    Fix Mailgun configuration

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ from flask.ext.heroku import Heroku
 app = Flask(__name__)
 heroku = Heroku(app)
 
-# email configuration
+# email configuration when running locally
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = '587'
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+# app.config["MAIL_PORT"] = '587'
+# app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = 'faustiesbakery@gmail.com'
 app.config["MAIL_PASSWORD"] = 'mueni1998'
+
+app.config['MAIL_SERVER'] = app.config['SMTP_SERVER']
+app.config['MAIL_USERNAME'] = app.config['SMTP_LOGIN']
+app.config['MAIL_PASSWORD'] = app.config['SMTP_PASSWORD']
 
 mail = Mail(app)
 
