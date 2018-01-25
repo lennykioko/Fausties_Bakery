2bc6bd37ae273bb4f161ff34f4c90edc26092840
unknown <lennykmutua@gmail.com>
2018-01-25 13:21:27 +0300

    Change from Django to Flask

---
+++ README.md
@@ -0,0 +1,54 @@
+![Home Image](https://raw.github.com/lennykioko/Fausties_Bakery/master/static/slide-1.jpg)
+
+
+
+[![PEP8](https://img.shields.io/badge/code%20style-pep8-orange.svg)](https://www.python.org/dev/peps/pep-0008/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Fausties_Bakery
+
+## Introduction
+Fausties_Bakery is a fully-functional real world project built using Flask with all the functionality to take orders and also contact the owner.
+
+### Quick Start
+
+1. Clone the repo
+  ```
+  $ git clone https://github.com/lennykioko/Fausties_Bakery.git
+  $ cd into the created folder
+  ```
+  
+2. Initialize and activate a virtualenv:
+  ```
+  $ virtualenv --no-site-packages env
+  $ source env/bin/activate
+  ```
+
+3. Install the dependencies:
+  ```
+  $ pip install -r requirements.txt
+  ```
+
+4. Run the development server:
+  ```
+  $ python app.py
+  ```
+
+5. Navigate to [http://localhost:5000](http://localhost:5000)
+
+
+## Built With
+* HTML5
+* CSS3
+* JavaScript
+* Bootstrap
+* Python 3.6.4 (PEP8)
+
+## Heroku link
+* https://fausties-bakery.herokuapp.com/
+
+## Author:
+Lenny Kioko.
+
+## License.
+The App uses an MIT license.
---
+++ app.py
@@ -0,0 +1,85 @@
+"""contains backend logic for the web app"""
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# email configuration
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = '587'
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = 'faustiesbakery@gmail.com'
+app.config["MAIL_PASSWORD"] = 'mueni1998'
+
+mail = Mail(app)
+
+
+@app.route('/')
+def index():
+    """displays the home page"""
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    """renders the about page"""
+    return render_template('about.html')
+
+@app.route('/prices')
+def prices():
+    """renders the prices page"""
+    return render_template('prices.html')
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    """renders the contact page and receives data from the contact form"""
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        site_message = request.form['site_message']
+
+        content = """ Name: {} \n Email: {} \n Phone: {} \n Message: {} \n
+                  """.format(name, email, phone, site_message)
+
+        msg = Message("NEW CLIENT MESSAGE",
+                      sender='faustiesbakery@gmail.com',
+                      body=content,
+                      recipients=['faustiesbakery@gmail.com'])
+        mail.send(msg)
+
+        return redirect(url_for('index'))
+
+    return render_template('contact.html')
+
+
+@app.route('/order', methods=['POST', 'GET'])
+def order():
+    """renders the order page and receives data from the order form"""
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cake_type = request.form['cake_type']
+        shape = request.form['shape']
+        weight = request.form['weight']
+        cake_message = request.form['cake_message']
+        instructions = request.form['instructions']
+
+        content = """ Name: {} \n Email: {} \n Phone: {} \n Cake_type: {} \n Shape: {} \n Weight: {} \n Cake_message: {} \n Instructions: {} \n
+                  """.format(name, email, phone, cake_type, shape, weight, cake_message, instructions)
+
+        msg = Message("NEW CLIENT ORDER",
+                      sender='faustiesbakery@gmail.com',
+                      body=content,
+                      recipients=['faustiesbakery@gmail.com'])
+        mail.send(msg)
+
+        return redirect(url_for('index'))
+
+    return render_template('order.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
